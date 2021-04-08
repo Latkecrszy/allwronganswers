31142486c348be36f452f89fc1d1cfea5cb07bb7
Latkecrszy <setharaphael@gmail.com>
2021-04-08 16:56:07 -0700

    updates

--- static/start.js
+++ static/start.js
@@ -5,7 +5,7 @@ async function showPlayers(id) {
     setInterval(async () => {
         let start_json = await fetch(`https://allwronganswers.com/players?id=${id}`)
         newPlayers = await start_json.json()
-        //newPlayers = [{'login_info': {'username': 'test'}}, {'login_info': {'username': 'test123'}}]
+        //newPlayers = [{'info': {'username': 'test'}}, {'info': {'username': 'test123'}}]
         if (JSON.stringify(newPlayers) != JSON.stringify(players)) {
             insert.innerHTML = null
             for (player of newPlayers) {
@@ -13,7 +13,7 @@ async function showPlayers(id) {
                 let username = document.createElement('p')
                 let x = document.createElement('div')
                 console.log("doing")
-                username.innerText = player['login_info']['username']
+                username.innerText = player['info']['username']
                 x.innerText = "×"
                 newPlayer.appendChild(username)
                 newPlayer.appendChild(x)
