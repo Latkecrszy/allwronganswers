3fc524e269780b1875b7759a1f7da707af20cb65
Latkecrszy <setharaphael@gmail.com>
2021-04-26 10:56:15 -0700

    testing:

--- static/start.js
+++ static/start.js
@@ -21,7 +21,7 @@ async function showPlayers(id) {
                 newPlayer.appendChild(username)
                 newPlayer.appendChild(x)
                 newPlayer.classList.add("container")
-                if (player['host'] === 'true') {newPlayer.style.backgroundColor = "#6bffc6"}
+                if (player['host'] === 'true') {newPlayer.style.backgroundColor = "#0026ff"}
                 insert.appendChild(newPlayer)
             }
             players = newPlayers
