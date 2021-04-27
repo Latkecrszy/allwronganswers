6e928e97f53ce5e9ed00b8c46780e4aa023766ba
Latkecrszy <setharaphael@gmail.com>
2021-04-27 11:28:37 -0700

    fixes

--- static/play.js
+++ static/play.js
@@ -1,7 +1,7 @@
 async function renderInfo(answers) {
     console.log(answers)
     for (let x = 1; x < 5; x++) {
-        let i = document.getElementById("question_"+x.toString())
+        let i = document.getElementById("answer_"+x.toString())
         console.log("question_"+x.toString())
         console.log(i)
         i.innerText = answers[x-1]
