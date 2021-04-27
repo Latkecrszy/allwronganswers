983258c2fdd88fb3a1f0f92d371ba1065bcd1be9
Latkecrszy <setharaphael@gmail.com>
2021-04-27 11:26:21 -0700

    testing

--- static/play.js
+++ static/play.js
@@ -2,6 +2,8 @@ async function renderInfo(answers) {
     console.log(answers)
     for (let x = 1; x < 5; x++) {
         let i = document.getElementById("question_"+x.toString())
+        console.log("question_"+x.toString())
+        console.log(i)
         i.innerText = answers[x-1]
         i.addEventListener("click", () => {i.classList.add("selected")})
     }
